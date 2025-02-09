c662c25ed543b0d56cbcf1a797704df10eafbfed
abdulraman-1 <abdulramanshopade@gmail.com>
2025-02-09 12:50:57 +0100

    fixed approute

--- src/Approute.jsx
+++ src/Approute.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
-import Header from "../components/Header";
-import Sidebar from "../components/Sidebar";
+import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
 
 const Approute = () => {
   return (
